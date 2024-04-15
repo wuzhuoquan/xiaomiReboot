7a864972f84b62ac0f095a605e6fa69beee94a62
wuzhuoquan <wuzhuoquan@yingmi.cn>
2024-04-15 10:19:26 +0800

    commit script

---
+++ xiaomi_reboot.py
@@ -0,0 +1,84 @@
+#!/usr/bin/python
+# -*-coding:utf-8-*-
+# Author: Five
+# CreatDate: 2024/4/15 10:18
+#
+
+
+import hashlib
+import json
+import random
+import re
+import requests
+import time
+import urllib.parse
+
+xmtime = int(time.time())
+xmrand = int(random.random()*10000)
+xmtype = 0
+xmpwd = ''
+ROUTE_IP = ""
+
+
+class Xiaomi(object):
+    def __init__(self, host):
+        self.host_ip = host
+        self.schema_host = "http://" + host
+        self.key = self.get_xmkey()[0]
+        self.deviceid = self.get_deviceid()
+
+    def get_xmkey(self):
+        url = self.schema_host + "/cgi-bin/luci/web"
+        resp = requests.get(url)
+        pattern_key =re.compile('key: \'(.*?)\'')
+        xmkey = re.findall(pattern_key, resp.text)
+        return xmkey
+
+    def get_deviceid(self):
+        url = self.schema_host + "/cgi-bin/luci/web"
+        resp = requests.get(url)
+        pattern_key = re.compile('var deviceId = \'(.*?)\'')
+        deviceid = re.findall(pattern_key, resp.text)
+        return deviceid
+
+    def make_nonece(self):
+        return "%s_%s_%s_%s" % (xmtype, self.deviceid, xmtime, xmrand)
+
+    def encry_passwd(self):
+        xmnonce = self.make_nonece()
+        xmsha1 = hashlib.sha1()
+        w = (xmpwd + self.key).encode()
+        xmsha1.update(w)
+        xmsha2 = hashlib.sha1()
+        xmsha2.update((xmnonce + xmsha1.hexdigest()).encode())
+        return xmsha2.hexdigest()
+
+    def login(self):
+        url = self.schema_host + "/cgi-bin/luci/api/xqsystem/login"
+        user_agent = 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2508.0 Safari/537.36 OPR/34.0.2023.0 (Edition developer)'
+        header = {'User-Agent': user_agent, 'Connection': 'keep-alive', 'Host': self.host_ip,
+                  'Referer': self.schema_host + '/cgi-bin/luci/web', "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8"}
+        values = {'username': 'admin', 'password': self.encry_passwd(), 'logtype': '2', 'nonce': self.make_nonece()}
+        data = urllib.parse.urlencode(values)
+        resp = requests.post(url, headers=header, data=data)
+        resp_json = json.loads(resp.text)
+        if resp_json.get("code") != 0:
+            print("login failed")
+            raise Exception("login failed")
+        path = resp_json.get("url")
+        return path
+
+    def reboot(self):
+        path = self.login()
+        path_list = path.split("/")
+        reboot_path = "/".join(path_list[0:4] + ["api", "xqsystem", "reboot"])
+        url = self.schema_host + reboot_path
+        params = {"client": "web"}
+        resp = requests.get(url, params=params)
+        print(resp.status_code)
+        print(resp.text)
+
+
+if __name__ == '__main__':
+    xiaomi = Xiaomi(ROUTE_IP)
+    xiaomi.reboot()
